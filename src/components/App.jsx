d069e5142b65c0456c8ffe7604ea7da9586dfc03
Ilyas Garipov <ilyasgaripov97@gmail.com>
2022-04-22 11:27:17 +0300

    Add basic navigation, react router dom.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
 import Wrapper from './containers/Wrapper';
 import Card from './containers/Card';
 
@@ -8,7 +13,19 @@ function App() {
   return (
     <div className="App">
       <Wrapper>
-        <Card>123</Card>
+        <Card>
+          <nav>
+            <Link class="navigation-item" to="/">Home</Link>
+            <Link class="navigation-item" to="/cart">Cart</Link>
+            <Link class="navigation-item" to="/profile">Profile</Link>
+          </nav>
+        </Card>
+
+        <Routes>
+          <Route path="/" element={<Card>home</Card>}/>
+          <Route path="/cart" element={<Card>cart</Card>}/>
+          <Route path="/profile" element={<Card>profile</Card>}/>
+        </Routes>
       </Wrapper>
     </div>
   );
