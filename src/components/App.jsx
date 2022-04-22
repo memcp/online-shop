815f0122540c5130ffef37e0aa5d503319c7d85f
Ilyas Garipov <ilyasgaripov97@gmail.com>
2022-04-22 10:42:37 +0300

    Add reset.css.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
     </div>
-  )
+  );
 }
 
 export default App;
