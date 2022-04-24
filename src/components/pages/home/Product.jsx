f7fd3c7e8c43fedccbf0a8c451a5291302f8eea2
Ilyas Garipov <ilyasgaripov97@gmail.com>
2022-04-24 10:15:43 +0300

    Add condition to not allow add an item twice if it already in cart.

--- src/components/pages/home/Product.jsx
+++ src/components/pages/home/Product.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-
+import { useDispatch, useSelector } from 'react-redux';
 import { createAddCartItemAction } from '../../../store/cart/actions';
 
 import './Product.css';
 
 function Product(props) {
   const { id, title, description, price } = props;
+  const { items } = useSelector((state) => state.cart)
   const dispatch = useDispatch();
 
   const handleClick = () => {
@@ -19,13 +19,22 @@ function Product(props) {
     dispatch(actionToCartAction);
   };
 
+  const alreadyInCart = (id) => {
+    return items.some(item => item.id === id);
+  }
+
   // TODO disable button if product already added to cart
   return (
     <div key={id} className="product">
       <h3>{title}</h3>
       <p>{description}</p>
       <p><span className="cost">Price: </span>{price}</p>
-      <button onClick={handleClick}>ADD TO CART</button>
+      <button
+        disabled={alreadyInCart(id) ? true : false}
+        onClick={handleClick}
+      >
+        ADD TO CART
+      </button>
     </div>
   );
 }
